290e2fd28954eb831fb79403a53fd8cac75e3fb1
konarsourav <sourav.konar@pfizer.com>
2021-04-20 18:22:08 +0530

    Add chatbot config file

---
+++ chatbot-config.js
@@ -0,0 +1,14 @@
+(function ($) {
+  'use strict';
+
+  window.CHATTERBOX_CONFIG = {
+name: 'Integration Test',
+botId: 'st-052f0ed6-de80-5f46-9be8-40a4935b078a',
+clientId: 'cs-284cc77d-0ad3-5cf2-882d-715d4db518b2',
+platformURL: 'https://chatterbox43.pfizer.com',
+middlewareURL: 'https://chatterbox49.pfizer.com',
+proxyHost: 'chatterbox43.pfizer.com',
+popOutMessage: 'Hello from IAS Chatbot',
+customData: {}
+}
+})(jQuery);
